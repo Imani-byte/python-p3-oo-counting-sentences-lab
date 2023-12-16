f3d535baf09d41040b53cbd4e8faaab9aac586ac
naisiae.kasura@gmail.com <imani.kasura@student.moringaschool.com>
2023-12-16 23:21:02 +0300

    completed assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value=""):
+    if not isinstance(value, str):
+      print("The value must be a string.")
+    self.value = value
+
+  def is_sentence(self):
+    return self.value.endswith('.')
+
+  def is_question(self):
+    return self.value.endswith('?')
+
+  def is_exclamation(self):
+    return self.value.endswith('!')
+
+  def count_sentences(self):
+    # Replace common sentence-ending punctuation with a period
+    normalized_string = self.value.replace('?', '.').replace('!', '.')
+        
+    # Split the string into sentences using '.' as the separator
+    sentences = normalized_string.split('.')
+        
+    # Count the number of non-empty sentences
+    count = sum(1 for sentence in sentences if sentence.strip())
+    return count
